6e04eaa6274e12bec9e2b748f3f658afa854f880
vincent <vincent@secondstate.io>
2023-09-18 16:39:50 +0800

    test webhook

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
-use http_req::request;
-use schedule_flows::schedule_cron_job;
-use serde_derive::{Deserialize, Serialize};
-
-use slack_flows::send_message_to_channel;
-use flowsnet_platform_sdk::logger;
+use webhook_flows::{create_endpoint, request_handler, send_response};
 
 #[no_mangle]
-pub fn run() {
-    logger::init();
-    let keyword = std::env::var("KEYWORD").unwrap();
-    println!("123");
-    schedule_cron_job(String::from("50 * * * *"), keyword, callback);
+#[tokio::main(flavor = "current_thread")]
+pub async fn on_deploy() {
+    create_endpoint().await;
 }
 
-fn callback(keyword: Vec<u8>) {
+#[request_handler]
+async fn handler(_headers: Vec<(String, String)>, _subpath: String, _qry: HashMap<String, Value>, _body: Vec<u8>) {
+    send_response(
+        200,
+        vec![(String::from("content-type"), String::from("text/html"))],
+        "ok".as_bytes().to_vec(),
+    );
 }
